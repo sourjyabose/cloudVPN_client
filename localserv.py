a9ea9eb12cdb3de2ccfdc260e28d70a8a017c4d5
Sourjya Basu <sourjyabose@gmail.com>
2026-08-30 15:45:07 +0530

    ok something is new

--- localserv.py
+++ localserv.py
@@ -8,7 +8,38 @@ import os
 import customtkinter
 import customtkinter as CTk
 import customtkinter as ctk
+import requests
+import os
+import hashlib
+import pickle
+import string
+from tkinter import messagebox
+from dotenv import load_dotenv
+import time
 
+go=0;
+validity=1;
+firstTimeRun=0;
+info={};
+load_dotenv()
+
+try:
+    info=pickle.load(open(".deviceInfo","rb"))
+except Exception as e:
+    firstTimeRun=1;
+    characters = string.ascii_letters + string.digits
+    
+    info["deviceId"]=''.join(random.choices(characters, k=16))
+
+
+url=os.getenv("ccserver")
+
+
+    
+
+def encodeNonce(email,value):
+    nonce=requests.get(url+"/nonce",json={"UEmail":email}).json()["nonce"]
+    return hashlib.sha256((value+str(nonce)).encode("utf-8")).hexdigest()
 
 
 def noprint(*args):
@@ -27,6 +58,9 @@ tabsarray=[]
 tabref={}
 
 def authsucc():
+    global go;
+    print("Go set")
+    go=1;
     for i in tabsarray:
         i.showframe();
 
@@ -35,21 +69,56 @@ def gettabframe(name):
 
 
 def gui():
+    
     loginwindow=None
+    email=0
+    passwd=0;
     def loginpage():
         nonlocal loginwindow;
         loginwindow=ctk.CTkToplevel(window);
         loginwindow.geometry("300x200")
         loginwindow.title("Login")
         loginwindow.attributes("-topmost",True)
-        authsucc()
+        #authsucc()
         
         CTk.CTkLabel(loginwindow,text="Username: ").grid(row=0,column=0,padx=5)
         CTk.CTkLabel(loginwindow,text="Password: ").grid(row=1,column=0,padx=5)
-        CTk.CTkEntry(loginwindow,placeholder_text="Enter Username",width=200).grid(pady=10,row=0,column=2)
-        CTk.CTkEntry(loginwindow,placeholder_text="Enter Password",width=200).grid(pady=10,row=1,column=2)
-        CTk.CTkButton(loginwindow,text="Login").grid(row=4,column=2)
+        email=CTk.CTkEntry(loginwindow,placeholder_text="Enter Email",width=200)
+        email.grid(pady=10,row=0,column=2)
+        passwd=CTk.CTkEntry(loginwindow,placeholder_text="Enter Password",width=200)
+        passwd.grid(pady=10,row=1,column=2)
+        CTk.CTkButton(loginwindow,text="Login",command=lambda: process(email.get(),passwd.get(),loginwindow)).grid(row=4,column=2)
         CTk.CTkLabel(loginwindow,text="No account? Sign Up Now !").grid(row=3,column=2)
+        
+    def process(email,passwd,loginwindow):
+        global url;
+        global info;
+        try:
+            response=requests.get(f"{url}/authenticate/{email}/{encodeNonce(email,passwd)}/{info["deviceId"]}")
+        except Exception as e:
+            messagebox.showerror("Error","Something went wrong") 
+        response=response.json()
+        if response["status"]=="success":
+            authdata[0]=response["data"]["email"]
+            authdata[1]=response["data"]["quota"]
+            authsucc()
+            if firstTimeRun==1:
+                info["email"]=email;
+                info["password"]=passwd;
+                pickle.dump(info,open(".deviceInfo","wb"))
+                if loginwindow!=None:
+                    loginwindow.destroy()
+        elif response["status"]=="ADR":
+            messagebox.showwarning("Another Device in Use","The account you are trying to log into is logged in on another device");
+            if messagebox.askokcancel("Remove Device?","Proceed to log put from another device ?") :
+                response=requests.get(f"{url}/changedevice/{email}/{encodeNonce(email,passwd)}/{info["deviceId"]}")
+                process(email,passwd,loginwindow)
+        elif response["status"]=="AuthFail":
+            messagebox.showerror("Authentication Failure","Invalid Email or Password")
+        elif response["status"]=="Blocked":
+            messagebox.showwarning("Account Deactivated","Your account has been deactivated for violating our policy.\n If you think this is a mistake contact us. Thank You.")
+            
+
         
 
 
@@ -72,6 +141,7 @@ def gui():
     window.geometry("1000x600")
     tabset=CTk.CTkTabview(window,width=950,height=550)
     tabset.pack()
+
 
     class createtabs:
         def __init__(self,name):
@@ -103,10 +173,17 @@ def gui():
     speed=CTk.StringVar()
     highestspeed=CTk.StringVar();
     quota=CTk.StringVar();
+    seconds=0;
+    def reporting():
+        requests.post()
     def update():
+            nonlocal seconds
             global prev;
+            seconds+=1;
+            if(seconds%30==0):
+                reporting();
             remdata.set(f"Reamaining Data: NaN")
-            quota.set(f"Quota: {authdata[1]}")
+            quota.set(f"Quota: {authdata[1]} GB")
             username.set(f"Username: {authdata[0]}")
             speed.set(f"Speed: {round(round(packetssofar/(1000*1000),2)-round(prev/(1000*1000),2),2):.2f}Mbps")
             prev=packetssofar;
@@ -119,7 +196,8 @@ def gui():
     CTk.CTkLabel(gettabframe("Usage Details"),textvariable=quota,compound="left",justify="left",anchor='w',width=100,font=('Arial',20)).grid(row=3+5,column=0,sticky='ew')
     update()
     #End Usage Tab
-
+    if firstTimeRun==0:
+        process(info["email"],info["password"],None)
     #authsucc()
 
 
@@ -244,6 +322,10 @@ def receivefromserverandsendtoclient(clts):
                 print("Connection closed from browser: ",payloadheader)
             #print(payloaddata.decode())
 dpq=[]
+
+while go==0:
+    time.sleep(0.001)
+
 for inx,servs in enumerate(servsocklist):
     clts=socket.socket()
     clts.connect(servs)
@@ -253,6 +335,7 @@ for inx,servs in enumerate(servsocklist):
     threading.Thread(target=senddatatoserver,args=(datapackets,clts)).start();
 
 #threading.Thread(target=receivefromserverandsendtoclient).start()
+
 while True:
     cl,addr1=sock.accept()
     threading.Thread(target=sendtoserverqueue,args=(cl,addr1,dpq[random.randint(1,20000)%len(dpq)])).start()
